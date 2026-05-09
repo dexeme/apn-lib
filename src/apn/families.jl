780dcf2dd78e7799d04085b48d25cf0e1e32f235
dexeme <dexe.me12@gmail.com>
2026-05-09 15:26:32 -0300

    Inicia código das famílias

---
+++ src/apn/families.jl
@@ -0,0 +1,28 @@
+using Nemo
+
+# https://boolean.w.uib.no/files/2018/07/quadratic_APN_poly.pdf
+
+function family_c1_c2()
+end
+
+function family_c3()
+end
+
+function family_c4()
+end
+
+function family_c5()
+end
+
+function family_c6()
+end
+
+function family_c7_c8_c9()
+end
+
+function family_c10()
+end
+
+function family_c11()
+end
+
